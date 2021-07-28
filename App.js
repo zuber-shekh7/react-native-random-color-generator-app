bf43f363c86c3bebad7cb8ee7a535d5de9d96481
Zuber Shekh <zuber.shekh7@gmail.com>
2021-07-28 20:46:04 +0530

    feat: add copy to clipboard feature

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Toast } from "react-native";
-import Button from "./src/components/Button";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Clipboard,
+} from "react-native";
 
+import Button from "./src/components/Button";
 import generateRandomColor from "./src/utils/generateRandomColor";
 
 const App = () => {
   const [pallteColor1, setPalletColor1] = useState(generateRandomColor());
   const [pallteColor2, setPalletColor2] = useState(generateRandomColor());
   const [pallteColor3, setPalletColor3] = useState(generateRandomColor());
+  const [selectedColor, setSelectedColor] = useState("");
 
   const onButtonPress = () => {
     setPalletColor1(generateRandomColor());
@@ -15,28 +22,41 @@ const App = () => {
     setPalletColor3(generateRandomColor());
   };
 
+  const copyToClipboard = (value) => {
+    Clipboard.setString(value);
+    selectedColor(value);
+  };
+
   return (
     <View style={styles.container}>
       {/* pallet1 */}
       <TouchableOpacity
         style={[styles.colorPallete, { backgroundColor: pallteColor1 }]}
+        onPress={() => setSelectedColor(pallteColor1)}
       >
         <View></View>
       </TouchableOpacity>
       {/* pallet2 */}
       <TouchableOpacity
         style={[styles.colorPallete, { backgroundColor: pallteColor2 }]}
+        onPress={() => setSelectedColor(pallteColor2)}
       >
         <View></View>
       </TouchableOpacity>
       {/* pallet3 */}
       <TouchableOpacity
         style={[styles.colorPallete, { backgroundColor: pallteColor3 }]}
+        onPress={() => setSelectedColor(pallteColor3)}
       >
         <View></View>
       </TouchableOpacity>
       {/* generate button */}
       <View style={styles.buttonContainer}>
+        <Text style={styles.subTitle}>
+          {selectedColor
+            ? `${selectedColor} is copied to Clipboard`
+            : "Top on pallete to copy the color code."}
+        </Text>
         <Button onPress={onButtonPress} title="Generate" />
         <Text style={styles.subTitle}>Tap on Generate to generate colors</Text>
       </View>
@@ -64,7 +84,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   subTitle: {
-    marginTop: 10,
+    marginVertical: 10,
     color: "grey",
   },
 });
